c4a965413fe59f13bbc73b97bee96bc71baf1f74
joanabranco <joanabranco.23@gmail.com>
2022-07-25 17:47:41 +0100

    one more project but is not finished yet

---
+++ Trivia Game/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,19 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+q
+Aapp/src/main/java/com/example/android/navigation/AboutFragment.kt,3\5\35410519d94a16b49ff7ddf19e54cbb105bcf034
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+t
+Dapp/src/main/java/com/example/android/navigation/GameOverFragment.kt,0\d\0d96c286d6f0ef84420e177aa62a64f8d5ba0db4
+s
+Capp/src/main/java/com/example/android/navigation/GameWonFragment.kt,d\a\da305b5380406c50ebd78c2eacb363d705fb42f6
+p
+@app/src/main/java/com/example/android/navigation/GameFragment.kt,f\1\f12f958c72d96423e307284f968c79b413371460
+p
+@app/src/main/java/com/example/android/navigation/MainActivity.kt,a\8\a802397259e241afcc723996888fbbf11402713b
+q
+Aapp/src/main/java/com/example/android/navigation/TitleFragment.kt,7\e\7e5bc462865e7e217e5006d06dd1df515de169cf
---
+++ Trivia Game/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,38 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+class TitleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding : FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title,container,false)
+        binding.playButton.setOnClickListener (Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
+                                            /* { view : View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+            // ou: Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
+        }*/
+        setHasOptionsMenu(true) //indica que o titlefragment tem menu
+        return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, view!!.findNavController()) || super.onOptionsItemSelected(item)
+    }
+
+}
